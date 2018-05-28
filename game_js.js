b199f0a3316bd07760247f70f51a71c287888d76
JohnsonPLU <36045410+JohnsonPLU@users.noreply.github.com>
2018-05-28 09:45:04 -0400

    Update game_js.js

--- game_js.js
+++ game_js.js
@@ -1,9 +1,36 @@
+var theButton = function() {
+  fill(255, 250, 0);
+  rect(900, 600, 200, 100);
+  fill(0, 0, 0);
+  textSize(25);
+  text("Start!", 950, 650);
+};
+
+var drawScene1 = function() {
+  background(200, 175, 175);
+  text("Select character", 200, 50);
+  
+}
+
 function setup() {
   createCanvas(1250, 900);
-background(92, 86, 92);    
+  background(92, 86, 92); 
+
+  theButton();
+  textSize(34);
+  text("The Ultimate Racer", 500, 450);
+};
+
+function mouseClicked() {
+  if (mouseX > 900 && mouseX < 900 + 200
+    && mouseY > 600 && mouseY < 700) {
+    background(98, 86, 92);
+
+
+    drawScene1();
+  }
 }
 
 function draw() {
-textSize(34);
-text("The Ultimate Racer", 500, 450);
-}
+
+};
