aada46a26db68db51c79725a393bbfa5e001f96c
JohnsonPLU <36045410+JohnsonPLU@users.noreply.github.com>
2018-05-31 11:45:44 -0400

    Update game_js.js

--- game_js.js
+++ game_js.js
@@ -13,7 +13,7 @@ var theButton = function() {
   rect(buttonx, buttony, 200, 100);
   fill(0, 0, 0);
   textSize(25);
-  text("Start!", 950, 650);
+  text("Start!", 965, 650);
 };
 
 var drawScene1 = function() {
@@ -23,56 +23,13 @@ var drawScene1 = function() {
   rect(250, 500, 200, 100);
   fill(0, 0, 0);
   textSize(25);
-  text("Select", 320, 550);
-};
-
-function setup() {
-  createCanvas(1250, 900);
-  background(92, 86, 92); 
-
-  theButton();
-  textSize(34);
-  text("The Ultimate Racer", 500, 450);
-};
-
-function mouseClicked() {
-  if (mouseX > 900 && mouseX < 900 + 200
-    && mouseY > 600 && mouseY < 700) {
-    background(98, 86, 92);
-
-
-    drawScene1();
-  }
-}
-
-function draw() {
-
-};var buttonx = 900;
-var buttony = 600;
-
-var button = {
-  x:900,
-  y:600
-}
-
-var theButton = function() {
-  
-  
-  fill(255, 250, 0);
-  rect(buttonx, buttony, 200, 100);
-  fill(0, 0, 0);
-  textSize(25);
-  text("Start!", 950, 650);
-};
-
-var drawScene1 = function() {
-  background(200, 175, 175);
-  text("Select character", 500, 150);
+  text("Character 1", 305, 560);
   fill(9, 255, 0);
-  rect(250, 500, 200, 100);
+  rect(750, 500, 200, 100);
   fill(0, 0, 0);
   textSize(25);
-  text("Select", 320, 550);
+  text("Character 2", 800, 560);
+  fill(0, 0, 0);
 };
 
 function setup() {
