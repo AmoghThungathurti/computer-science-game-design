dfa6ff25715750348f8edb98395d94bd740bb551
AmoghThungathurti <36045681+AmoghThungathurti@users.noreply.github.com>
2018-06-08 09:32:48 -0400

    Update track.js

--- track.js
+++ track.js
@@ -1,9 +1,10 @@
 function setup() {
-createCanvas(1250, 950)
-background(0, 0, 0)
+createCanvas(1250, 900)
+background(255, 255, 255)
 }
 
 function draw() {
+  fill(0, 0, 0)
   rect(200, 200, 700, 400)
   fill(89, 89, 89)
   rect(350, 300, 350, 200)
